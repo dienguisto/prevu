637147a89fa801e8b888bab23b5d63073943edcc
nmtsylla <niamatosylla@gmail.com>
2015-03-21 00:03:16 +0000

    Update template list/add

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,27 @@ module ApplicationHelper
       "<span class='badge badge-warning'>#{no}</span>"
     end.html_safe
   end
+
+  def afficher_status(status)
+    if status == 1
+      "<span class='badge badge-default'>En attente</span>"
+    elsif status == 2
+      "<span class='badge badge-success'>Actif</span>"
+    elsif status == 3
+      "<span class='badge badge-warning'>Suspendu</span>"
+    elsif status == 4
+      "<span class='badge badge-danger'>Supprimé</span>"
+    end.html_safe
+  end
+
+  def set_link(model, action, id)
+    id = id.to_s
+    if action == 'show'
+      "<a href='/"+model+"/"+id+"'><i class='fa fa-check text-navy'></i></a>"
+    elsif action == 'edit'
+      "<a href='/"+model+"/"+id+"/edit'><i class='fa fa-edit text-navy'></i></a>"
+    elsif action == 'remove'
+      "<a data-confirm='Are you sure?' rel='nofollow' data-method='delete' href='/"+model+"/"+id+"><i class='fa fa-delete text-navy'></i></a>"
+    end.html_safe
+  end
 end
