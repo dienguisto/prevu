b08190361d30aabf35e7a17373d94138e2aecfdb
nmtsylla <niamatosylla@gmail.com>
2015-03-22 20:47:00 +0000

    disallow empty detail ordonnance

--- app/models/ordonnance.rb
+++ app/models/ordonnance.rb
@@ -5,8 +5,14 @@ class Ordonnance < ActiveRecord::Base
   has_many :detail_ordonnances, dependent: :destroy
   has_many :medicaments, through: :detail_ordonnances
 
+  before_create :set_total
   accepts_nested_attributes_for :detail_ordonnances,
-      :allow_destroy => true
+      :allow_destroy => true, :reject_if => :all_blank
 
   #validates_presence_of :detail_ordonnances
+
+  private
+    def set_total
+
+    end
 end
