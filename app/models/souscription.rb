037b2ce6aa5d27597ef0fa69d00fe4fd8aaa7ac9
sidya <scicasoft@gmail.com>
2015-03-25 15:37:10 +0000

    set_date_paiement

--- app/models/souscription.rb
+++ app/models/souscription.rb
@@ -10,11 +10,17 @@ class Souscription < ActiveRecord::Base
 
   scope :en_cours, -> { where('date_expiration >= ?', Time.now) }
 
+  before_create :set_date_paiement
+
   def activate!
     update(actif: true, date_activation: Time.now)
   end
 
   def payer!
     update(paye: true, date_paiement: Time.now, date_expiration: Time.now + 1.year)
+  end
+
+  def set_date_paiement
+    self.date_paiement = Time.now if paye?
   end
 end
