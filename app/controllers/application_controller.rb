b1c68683ae5ff7c1f953985cae2bedb853153116
sidya <scicasoft@gmail.com>
2015-04-10 17:28:21 +0000

    dashboard pharmacie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_adherent, :current_structure_assurance
+  helper_method :current_adherent, :current_structure_assurance, :current_pharmacy, :current_formation_sanitaire
 
   before_action :authenticate_user!
   layout :layout_by_resource
--- app/models/pharmacy.rb
+++ app/models/pharmacy.rb
@@ -6,4 +6,12 @@ class Pharmacy < ActiveRecord::Base
   def title
     "Pharmacie - #{nom}"
   end
+
+  def montant_engage(date=nil)
+    if date.nil?
+      ordonnances.reduce(0) {|s, o| s + o.montant_pris_en_charge}
+    else
+      ordonnances.where('DATE(created_at)=?', date).reduce(0) {|s, o| s + o.montant_pris_en_charge}
+    end
+  end
 end
