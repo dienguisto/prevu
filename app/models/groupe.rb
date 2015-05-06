cd1d4311535600c5ed48be9a12a881331b161867
sidya <scicasoft@gmail.com>
2015-05-06 14:49:42 +0000

    afficher le total des transactions de chaque groupements

--- app/models/groupe.rb
+++ app/models/groupe.rb
@@ -8,4 +8,22 @@ class Groupe < ActiveRecord::Base
   has_many :adherents
 
   validates :nom, presence: true, uniqueness: true
+
+  def total_a_verser_pharmacies(date=nil)
+    ordonnances = Ordonnance.where(adherent: adherents)
+    if date.nil?
+      ordonnances.reduce(0) {|s, o| s + o.montant_pris_en_charge}
+    else
+      ordonnances.where('DATE(created_at)=?', date).reduce(0) {|s, o| s + o.montant_pris_en_charge}
+    end
+  end
+
+  def total_a_verser_structure_sanitaires(date=nil)
+    consultations = Consultation.where(adherent: adherents)
+    if date.nil?
+      consultations.reduce(0) {|s, c| s + c.montant_pris_en_charge}
+    else
+      consultations.where('DATE(created_at)=?', date).reduce(0) {|s, c| s + c.montant_pris_en_charge}
+    end
+  end
 end
